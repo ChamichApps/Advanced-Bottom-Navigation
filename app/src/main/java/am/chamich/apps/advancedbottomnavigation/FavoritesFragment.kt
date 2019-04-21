a8d212bf98c6aba3d26b1e4c62394b787762e2d5
Viktor Apoyan <viktor@opus.am>
2019-04-21 22:12:44 +0200

    [x] Icons and Titles of Navigation Bottom Bar changed [x] Home, Favorites and Profile fragments added [x] Logs added to the fragments lifecycle methods [x] Navigation Graph added [x] Bottom Navigation Bar using Navigation Graph to navigate ---- [-] On each navigation fragment is recreated

---
+++ app/src/main/java/am/chamich/apps/advancedbottomnavigation/FavoritesFragment.kt
@@ -0,0 +1,45 @@
+package am.chamich.apps.advancedbottomnavigation
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class FavoritesFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.i("~~~", "$TAG[$this] - onCreate()")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.i("~~~", "$TAG[$this] - onViewCreated()")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        Log.i("~~~", "$TAG[$this] - onDestroyView()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("~~~", "$TAG[$this] - onDestroy()")
+    }
+
+    companion object {
+        private val TAG = FavoritesFragment::class.java.simpleName
+    }
+}
